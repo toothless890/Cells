df6d6261bb728db1fea0175bc8df3eb11c5aede7
toothless890 <ashton.minden@gmail.com>
2023-02-12 20:49:13 -0800

    fixed some x and y swapping

--- Cell.java
+++ Cell.java
@@ -22,8 +22,10 @@ public class Cell {
     public void updateGroup(Canvas canvas) { // Canvas is a matrix of Cells
         for (Integer x = -1; x <= 1; x++) {
             for (Integer y = -1; y <= 1; y++) {
-                Integer[] position = { this.position[0] + x, this.position[0] + y };
-                this.group[x + 1][y + 1] = canvas.getCell(position);
+                Integer[] position = { this.position[0] + y, this.position[1] + x };
+                if (MatrixOperations.checkBounds(position, canvas)) {
+                    this.group[y + 1][x + 1] = canvas.getCell(position);
+                }
             }
         }
     }
@@ -31,13 +33,16 @@ public class Cell {
     public void replicate(Canvas canvas) {
         Integer[] workingMatrixes = { 0, 0, 0, 0 };
         int divisions = 0;
+
         for (int m = 0; m < 4; m++) {
             for (int r = 0; r < 3; r++) {
                 for (int i = 0; i < 3; i++) { // this is super overcomplicated, try to use Constants.testMatrixPositions
                                               // or something
                     if (Constants.testMatrixes[m][r][i] == 1) {
+
                         if (this.group[r][i] == null) {
                             workingMatrixes[m] += 1;
+
                             if (workingMatrixes[m] == 2) {
                                 divisions += 2;
                             }
@@ -48,14 +53,20 @@ public class Cell {
             }
         }
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 4; i++) {
             if (workingMatrixes[i] > 1) {
-                Integer[] bpos = MatrixOperations.add2IntList(Constants.testMatrixPositions[i][0], this.position);
-                Cell a = new Cell(bpos, divisions, i);
-                canvas.addCell(a);
-                Integer[] apos = MatrixOperations.add2IntList(Constants.testMatrixPositions[i][1], this.position);
-                Cell b = new Cell(apos, divisions, i);
-                canvas.addCell(b);
+
+                Integer[] apos = MatrixOperations.add2IntList(Constants.testMatrixPositions[i][0], this.position);
+                if (MatrixOperations.checkBounds(apos, canvas)) {
+                    Cell a = new Cell(apos, divisions, i);
+                    canvas.planCell(a);
+                }
+
+                Integer[] bpos = MatrixOperations.add2IntList(Constants.testMatrixPositions[i][1], this.position);
+                if (MatrixOperations.checkBounds(bpos, canvas)) {
+                    Cell b = new Cell(bpos, divisions, i);
+                    canvas.planCell(b);
+                }
             }
         }
 
