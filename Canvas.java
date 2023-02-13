2af68be90ab20e4e69dcb07e2373a8c2a28ff343
toothless890 <ashton.minden@gmail.com>
2023-02-12 20:48:50 -0800

    Update Canvas.java
    Fixed some issues with x and y getting swapped, added an updatecell function so that it's not trying to calculate the next replication during the current update cycle

--- Canvas.java
+++ Canvas.java
@@ -1,14 +1,15 @@
 public class Canvas {
 
     private Cell[][] cells;
-
-    private int width;
-    private int height;
+    private Cell[][] plannedCells;
+    int width;
+    int height;
 
     Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.cells = new Cell[this.width][this.height];
+        this.cells = new Cell[this.height][this.width];
+        this.plannedCells = new Cell[this.height][this.width];
     }
 
     public Cell getCell(Integer[] pos) {
@@ -24,9 +25,20 @@ public class Canvas {
         this.cells[pos[0]][pos[1]] = null;
     }
 
-    public void addCell(Cell cell) {
+    public void planCell(Cell cell) {
+        Integer[] pos = cell.getPosition();
+        this.plannedCells[pos[0]][pos[1]] = cell;
+    }
+
+    public void addCell(Cell cell) { // should only be used for cell creation, for new cells via replication, and
+                                     // movement, use planCell
         Integer[] pos = cell.getPosition();
         this.cells[pos[0]][pos[1]] = cell;
+    }
+
+    public void updateCells() {
+        this.cells = this.plannedCells;
+        this.plannedCells = new Cell[this.height][this.width];
     }
 
 }
