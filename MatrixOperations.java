c87943e95dba16ccc99c73432996ef04e122efb2
toothless890 <ashton.minden@gmail.com>
2023-02-12 18:58:50 -0800

    Create MatrixOperations.java
    may add more in the future, wanted a way to add two Integer[]'s together

---
+++ MatrixOperations.java
@@ -0,0 +1,11 @@
+public class MatrixOperations {
+
+    public static Integer[] add2IntList(Integer[] a, Integer[] b) {
+        Integer[] sum = new Integer[2];
+        sum[0] = a[0] + b[0];
+        sum[1] = a[1] + b[1];
+
+        return sum;
+    }
+
+}
