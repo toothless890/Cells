68a67535e66caa4124aa7f084afb967cc9639a2c
toothless890 <ashton.minden@gmail.com>
2023-02-12 18:59:14 -0800

    testing replication, works well so far

--- Run.java
+++ Run.java
@@ -1,3 +1,30 @@
-public class Run {
+import java.io.*;
+import java.util.Arrays;
 
+public class Run {
+    public static void print(Object message) {
+        System.out.print(message);
+    }
+
+    public static void println(Object message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Canvas canvas = new Canvas(3, 3);
+        Integer[] testpos = { 1, 1 };
+        Cell Brian = new Cell(testpos, 100, 100);
+        canvas.addCell(Brian);
+        Brian.replicate(canvas);
+        String[] a = Arrays.deepToString(canvas.getAllCells()).split(",", 0);
+        int x = 0;
+        for (String list : a) {
+            print(list);
+            x = (x + 1) % 3;
+            if (x == 0) {
+                print("\n");
+            }
+        }
+        // print();
+    }
 }
